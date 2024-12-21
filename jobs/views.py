ad2a1095f371d5bd8e5c2dbaec31767f873a5bc4
hariharen96 <hariharen1996@gmail.com>
2024-12-21 18:22:01 +0530

    style: add responsive navbar, job_home page

--- jobs/views.py
+++ jobs/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def job_home(request):
-    return HttpResponse("<h1>Home</h1>")
+    return render(request,'jobs/jobs_home.html',{'title':"jobs_home"})
