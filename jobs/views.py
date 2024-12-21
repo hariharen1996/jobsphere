0324dfd32a0d70714ad8673d82f9d36f27a2a6ee
hariharen96 <hariharen1996@gmail.com>
2024-12-21 14:06:00 +0530

    feat: add users,job app in settings and setup basic url routing

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def job_home(request):
+    return HttpResponse("<h1>Home</h1>")
--- jobsphere/settings.py
+++ jobsphere/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_bootstrap5'
+    'django_bootstrap5',
+    'users',
+    'jobs'
 ]
 
 MIDDLEWARE = [
