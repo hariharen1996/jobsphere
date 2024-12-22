a7e44deb599232659b6c2cd62b33431d5c5031b9
hariharen96 <hariharen1996@gmail.com>
2024-12-22 11:07:47 +0530

    feat: add login register routing setup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_login(request):
+    return render(request,'users/login.html',{'title': 'LoginPage'})
+
+
+def user_register(request):
+    return render(request,'users/register.html',{'title': 'RegisterPage'})
