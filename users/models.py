217b72fefe279ca99b7caaf4ba6588f0d3975b3f
hariharen96 <hariharen1996@gmail.com>
2024-12-24 13:02:12 +0530

    feat: update profile modal with additional details

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -12,10 +13,62 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Certification(models.Model):
+    name = models.CharField(max_length=255)
+    cert_image = models.ImageField(upload_to='certifications/')
+
+    class Meta:
+        verbose_name = "Certification"
+        verbose_name_plural = "Certifications"
+
+    def __str__(self):
+        return self.name
+
+class SkillCategory(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+
+    class Meta:
+        verbose_name = "Skill Category"
+        verbose_name_plural = "Skill Categories"
+    
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(SkillCategory,on_delete=models.CASCADE,related_name="skills",default=1)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name','category'],name="unique_skill_category")
+        ]
+
+    def __str__(self):
+        return self.name   
 
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pic')
+    bio = models.TextField(default="No bio available")
+    education = models.CharField(max_length=255,blank=False,null=False,default="Not Specified")
+    cgpa = models.DecimalField(max_digits=4,decimal_places=2,default=0.0)
+    work_experience = models.TextField(default="No work experince available")
+    resume = models.FileField(upload_to='resumes/',default="resumes/default_resume.pdf")
+    location = models.CharField(max_length=255,default="Not Specified")
+    certification = models.ManyToManyField(Certification,related_name='profiles')
+    skills = models.ManyToManyField(Skill)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def clean(self):
+        total_cert = self.certification.count()
+
+        if total_cert > 5:
+            raise ValidationError('A profile can only have 5 certifications')   
 
     def __str__(self):
         return f"{self.user.username} profile"
