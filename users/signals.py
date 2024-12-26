1c619056b2ddf3132ee6902abd2f708d608a548a
hariharen96 <hariharen1996@gmail.com>
2024-12-26 09:05:56 +0530

    refactor: modify signals based on user_type

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save,post_migrate
 from django.dispatch import receiver 
 from django.contrib.auth import get_user_model 
 from .models import Profile
+from jobs.models import Employer
 
 
 CustomUser = get_user_model()
@@ -9,15 +10,27 @@ CustomUser = get_user_model()
 @receiver(post_save,sender=CustomUser)
 def create_profile(sender,instance,created,**kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        if instance.user_type == 'Applicant':
+            Profile.objects.get_or_create(user=instance)
+        elif instance.user_type == 'Employer':
+            Employer.objects.get_or_create(user=instance)
 
-@receiver(post_save,sender=CustomUser)
-def save_profile(sender,instance,**kwargs):
-    if hasattr(instance,'profile'):
-        instance.profile.save()
+
+# @receiver(post_save,sender=CustomUser)
+# def save_profile(sender,instance,**kwargs):
+#     if instance.user_type == 'Applicant' and not hasattr(instance,'profile'):
+#         Profile.objects.get_or_create(user=instance)
+#     if instance.user_type == 'Employer' and not hasattr(instance,'employer'):
+#         Employer.objects.get_or_create(user=instance)
+    
 
 @receiver(post_migrate)
 def create_existing_user_profile(sender,**kwargs):
-    user_profile = CustomUser.objects.filter(profile__isnull=True)
-    for user in user_profile:
-        Profile.objects.create(user=user)        
+    applicant_user_profile = CustomUser.objects.filter(profile__isnull=True,user_type='Applicant')
+    for user in applicant_user_profile:
+        Profile.objects.get_or_create(user=user)        
+    
+    recruiter_user_profile = CustomUser.objects.filter(employer__isnull=True,user_type='Recruiter')
+    for user in recruiter_user_profile:
+        Employer.objects.get_or_create(user=user)        
+    
